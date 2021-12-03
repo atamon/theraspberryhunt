b10a182c43bb8b83f3ce6af470d03004f1e69dac
Anton Lindgren <anton.lindgren@yolean.com>
2021-12-03 08:17:21 +0100

    Muck around with image paths and inclusions

--- src/Boat.jsx
+++ src/Boat.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-// @ts-ignore
-import boat from './boat.png';
 
 export default function Boat({ colors = true }) {
 
   /** @type {React.CSSProperties} */
   const style = {
     filter: colors ? '' : 'grayscale(100%)',
-    backgroundImage: `url(${boat})`,
-    backgroundSize: 'contain'
   };
 
   return (
